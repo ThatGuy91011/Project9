c4f03497f2c6c6e7c7edda1e1120d95250359f01
KIRK WALLS <KIRWALLS@uat.edu>
2019-11-12 13:15:38 -0700

    Initial Commit

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,18 @@
- 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int age = 21;
+
+	int& reference = age;
+
+	cout << "My age is " << age << endl;
+	cout << "Your age is " << reference << endl;
+
+	reference = 18;
+
+	cout << "My age is " << age << endl;
+	cout << "Your age is " << reference << endl;
+}
